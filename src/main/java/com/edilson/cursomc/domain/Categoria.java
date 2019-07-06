5e024ddc41ede949c8d868dd2b8afbd32b2960a2
Edilson da Paixao <007edi007@gmail.com>
2019-07-05 21:17:25 -0300

    Criando repository e service para categoria

--- src/main/java/com/edilson/cursomc/domain/Categoria.java
+++ src/main/java/com/edilson/cursomc/domain/Categoria.java
@@ -2,7 +2,7 @@ package com.edilson.cursomc.domain;
 
 import java.io.Serializable;
 
-import javax.annotation.Generated;
+//import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
