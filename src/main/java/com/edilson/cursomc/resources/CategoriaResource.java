e17f9c3b05a1404251b43560dc64c5d241fdf782
Edilson da Paixao <007edi007@gmail.com>
2019-07-21 11:38:04 -0300

    Usando DTO

--- src/main/java/com/edilson/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/edilson/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.edilson.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 //import java.util.ArrayList;
 //import java.util.List;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.edilson.cursomc.domain.Categoria;
+import com.edilson.cursomc.dto.CategoriaDTO;
 import com.edilson.cursomc.services.CategoriaService;
 
 @RestController
@@ -52,4 +55,14 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+		
+	}
+	
 }
--- src/main/java/com/edilson/cursomc/services/CategoriaService.java
+++ src/main/java/com/edilson/cursomc/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.edilson.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -41,6 +43,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
 		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 	
 }
