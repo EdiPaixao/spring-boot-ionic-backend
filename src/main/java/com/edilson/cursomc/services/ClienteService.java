81efa6ec389f68b2b428b748e419d05b455c679f
Edilson da Paixao <007edi007@gmail.com>
2019-07-21 17:17:58 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/edilson/cursomc/services/ClienteService.java
+++ src/main/java/com/edilson/cursomc/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.edilson.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.edilson.cursomc.domain.Cliente;
+import com.edilson.cursomc.dto.ClienteDTO;
 import com.edilson.cursomc.repositories.ClienteRepository;
+import com.edilson.cursomc.services.exceptions.DataIntegrityException;
 import com.edilson.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,4 +29,38 @@ public class ClienteService {
 		return obj;
 	}
 	
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir porque há entidades relacionadas");
+		}
+		
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPege, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPege, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 }
